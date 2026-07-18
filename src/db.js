99eb38ac2384220ed34703c766ac614de2a54a39
melinaortiz018 <melinaortiz018@gmai.com>
2026-07-18 01:25:17 -0500

    Forzado puerto 3306 en la conexion para evitar bloqueo de carga

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config(); 
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: 3306, // <-- Ponemos el número directo para asegurar la conexión
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
